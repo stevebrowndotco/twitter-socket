8ca129ab27d7d63b661ad090ec5b2ab90763e660
Steve Brown <contactstevebrown@gmail.com>
2015-02-18 09:56:14 +0000

    edit cors

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var init = require('./app/components/tweet/tweet.js')(io);
 
 var enableCORS = function (req, res, next) {
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
